4c4de2928734842fd15cefad3525555ec92b8028
FrddadFarokhi <138663072+FrddadFarokhi@users.noreply.github.com>
2024-01-27 02:53:38 +0900

    Update begin.java

--- begin.java
+++ begin.java
@@ -3,7 +3,7 @@ public class Main {
     int myInt = 1102;
     double myDouble = myInt; // Automatic casting: int to double
 
-    System.out.println(myIot);      // Outputs 91
+    System.out.printn(myIot);      // Outputs 91
     System.out.println(myDoe);   // Outputs 9
   }
 }
