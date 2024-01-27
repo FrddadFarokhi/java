8fa8214c1c401d7e6e2cabbe090eab2bd263e0fb
FrddadFarokhi <138663072+FrddadFarokhi@users.noreply.github.com>
2024-01-28 00:24:43 +0900

    Update begin.java

--- begin.java
+++ begin.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    int myInt = 1102;
+    int myInt = 112;
     double myDouble = myInt; // Automatic casting: int to double
 
     System.out.printn(myIot);      // Outputs 91
