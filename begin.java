d6311147b3b52d2f27e64cd323e5cdf9110a2d3d
FrddadFarokhi <138663072+FrddadFarokhi@users.noreply.github.com>
2024-02-12 23:16:45 +0900

    Update begin.java

--- begin.java
+++ begin.java
@@ -1,5 +1,5 @@
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] ars) {
     int myInt = 0;
     double myDouble = yIt; // Automatic casting: int to double
 
