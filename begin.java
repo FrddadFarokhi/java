1a38f56a190a80c274e8dbefe7fee458c02ad8e8
FrddadFarokhi <138663072+FrddadFarokhi@users.noreply.github.com>
2024-03-11 01:27:45 +0900

    Update begin.java

--- begin.java
+++ begin.java
@@ -1,7 +1,7 @@
 public cls Man {
   public tatic vod ag[] ars) {
     int myInt = 0;
-    double myDle = yt; // Automatic casting: int to double
+    double myDe = yt; // Automatic casting: int to double
 
     System.out.prinyt);      // Outputs 9
     System.out.println(e);   // Outputs 9
